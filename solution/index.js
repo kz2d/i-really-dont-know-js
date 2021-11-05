3e73a82138fc7cc09b6e9a490b1052938551a422
kz2d <avidex111@gmail.com>
2021-11-05 07:13:39 -0900

    oh my god is it a solution folder

---
+++ solution/index.js
@@ -0,0 +1,54 @@
+class MySet {
+    object = []
+    constructor(array = []) {
+        for (const i of array) {
+            this.add(i)
+        }
+    }
+    has(item) {
+        return this.object.filter((a) => a === item).length > 0
+    }
+
+    add(item) {
+        if (!this.has(item))
+            this.object.push(item)
+        return this
+    }
+    delete(item) {
+        this.object = this.object.filter((a) => a !== item)
+    }
+
+    clear() {
+        this.object = []
+    }
+    get size() {
+        return this.object.length
+    }
+
+    entries() {
+        let tmp = []
+        for (const i of this.object) {
+            tmp.push([i, i])
+        }
+        return tmp
+    }
+    forEach(cb, data) {
+        let that = data || this
+        for (const i of this.object) {
+            cb.call(that, i)
+        }
+    }
+    [Symbol.iterator]() {
+        var index = -1;
+        var object = this.object;
+
+        return {
+            next: () => ({ value: object[++index], done: !(index in object) })
+        };
+    };
+    [Symbol.toStringTag] = 'Set'
+}
+
+
+module.exports = MySet;
+
