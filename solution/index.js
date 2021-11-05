41fb78b7d65e89623f622a8f275d713da70689e7
kz2d <avidex111@gmail.com>
2021-11-06 00:01:51 +0300

    new

--- solution/index.js
+++ solution/index.js
@@ -1,54 +1,104 @@
 module.exports = class MySet {
-    object = []
-    constructor(array = []) {
-        for (const i of array) {
-            this.add(i)
-        }
+  constructor(array = []) {
+    this._object = [];
+    for (const i of array) {
+      this.add(i);
     }
-    has(item) {
-        return this.object.includes(item)
+  }
+  has(item) {
+    return this._object.includes(item);
+  }
 
-    }
+  add(item) {
+    if (!this.has(item)) this._object.push(item);
+    return this;
+  }
+  delete(item) {
+    this._object = this._object.filter((a) => a !== item);
+  }
+  clear() {
+    this._object = [];
+  }
+  get size() {
+    return this._object.length;
+  }
 
-    add(item) {
-        if (!this.has(item))
-            this.object.push(item)
-        return this
+  *entries() {
+    for (const i of this._object) {
+      yield [i, i];
     }
-    delete(item) {
-        this.object.filter((a) => a !== item)
+  }
+  forEach(cb, data) {
+    // this.object.forEach(cb, data);
+    let that = data || this;
+    for (const i of this._object) {
+      cb.call(that, i);
     }
+  }
+  [Symbol.iterator]() {
+    return this._object.values();
+  }
+  get [Symbol.toStringTag]() {
+    return "^_^";
+  }
+};
 
-    clear() {
-        this.object = []
-    }
-    get size() {
-        return this.object.length
-    }
+// const MySet = module.exports;
 
-    entries() {
-        let tmp = []
-        for (const i of this.object) {
-            tmp.push([i, i])
-        }
-        return tmp.values()
-    }
-    forEach(cb, data) {
-        this.object.forEach(cb, data)
-        // let that = data || this
-        // for (const i of this.object) {
-        //     cb.call(that, i)
-        // }
-    }
-    [Symbol.iterator]() {
-        var index = -1;
-        var object = this.object;
+// // тесты
+// const set = new MySet([4, 8, 15, 15, 16, 23, 42]);
 
-        return {
-            next: () => ({ value: object[++index], done: !(index in object) })
-        };
-    };
-    get [Symbol.toStringTag]() { return '^_^' }
+// // хранит только уникальные значения
+// console.log([...set]); // [ 4, 8, 15, 16, 23, 42 ]
 
-}
+// // есть свойство size
+// console.log(set.size); // 6
 
+// // работает в цикле for-of
+// for (const item of set) {
+//   console.log(item); // 4 8 15 16 23 42
+// }
+
+// // есть методы keys, values, entries
+// for (const item of set.entries()) {
+//   console.log(item); // [ 4, 4 ] [ 8, 8 ] ...
+// }
+
+// // есть метод clear
+// set.clear();
+// console.log(set.size); // 0
+
+// const object = {
+//   getValue() {
+//     return this.value;
+//   },
+// };
+
+// const data = {
+//   value: 42,
+// };
+
+// // есть метод add
+// set.add(object);
+// set.add(data);
+
+// // который может работать в цепочке вызовов
+// set.add(object).add(object).add(object);
+
+// // есть метод delete
+// set.delete(data);
+
+// // есть метод has
+// console.log(set.has({})); // false
+// console.log(set.has(object)); // true
+// console.log(set.has(data)); // false
+
+// // и кое-что еще
+// console.log(set === set.valueOf()); // true
+// console.log(String(set)); // [object ^_^]
+// console.log(Object.prototype.toString.call(set)); // [object ^_^]
+
+// // есть forEach, который делает какие-то странные вещи...
+// set.forEach(function (item) {
+//   console.log(item.getValue.call(this)); // 42
+// }, data);
